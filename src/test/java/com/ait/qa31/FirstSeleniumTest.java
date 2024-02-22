63e654745c06ce33a338c46edbbba6653c4744cb
Tetiana Ilienko <tatyanailyenko@gmail.com>
2024-02-22 14:12:23 +0100

    first commit

---
+++ src/test/java/com/ait/qa31/FirstSeleniumTest.java
@@ -0,0 +1,31 @@
+package com.ait.qa31;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+    WebDriver driver;
+
+    // before - setUp
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+    }
+
+    //test
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Google opened!");
+    }
+    @Test
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+}
